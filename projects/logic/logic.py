e7fbcd014fb47bb2c34d69545e1c933b4590eb5d
FredslundMagnus <s183905@student.dtu.dk>
2022-07-16 22:55:12 +0200

    sd

---
+++ projects/logic/logic.py
@@ -0,0 +1,11 @@
+
+class Symbol:
+	pass
+
+# prefix ¬(x: Symbol) -> Symbol: return x.negate() 
+
+def macro_0(x: str) -> Symbol: return 
+def macro_1(x: str) -> Symbol: return 
+
+
+macro_0('x')(x.__circle_cross__(¬x))
